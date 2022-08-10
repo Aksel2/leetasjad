016932396535074b55f70e09915b7dd8028957bf
Aksel Õim <akseloim@gmail.com>
2022-08-10 19:29:13 +0300

    rlahendus

--- Solution4.java
+++ Solution4.java
@@ -2,12 +2,27 @@ import java.util.Arrays;
 
 class Solution4 {
     public static int[] twoSum(int[] nums, int target) {
+        int[] targetNums = new int[2];
+        boolean stop = false;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (i != j && nums[i] + nums[j] == target) {
+                    targetNums[0] = i;
+                    targetNums[1] = j;
+                    stop = true;
+                    break;
+                }
+            }
+            if (stop)
+                break;
+        }
 
-        return new int[0];
+        return targetNums;
     }
-    public static void main(String[] args){
-        int[] values = {2,7,11,15};
-        System.out.println(Arrays.toString(twoSum(values, 9)));
+
+    public static void main(String[] args) {
+        int[] values = {2, 5, 5, 11};
+        System.out.println(Arrays.toString(twoSum(values, 10)));
 
     }
 }
