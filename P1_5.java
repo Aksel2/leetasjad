6e8e8de3fc76dad4628b2b80c2f09a0d907a7306
Aksel Õim <akseloim@gmail.com>
2022-08-15 18:30:02 +0300

    Lahendused

--- P1_5.java
+++ P1_5.java
@@ -1,31 +1,25 @@
 public class P1_5 {
     public static void printRectangle(int size) {
         for (int i = 0; i < size; i++) {
-            if (i == 0 || i == size - 1)
-                System.out.println("#".repeat(size));
-            else
-                System.out.println("#" + " ".repeat(size - 2) + "#");
+            if (i == 0 || i == size - 1) System.out.println("#".repeat(size));
+            else System.out.println("#" + " ".repeat(size - 2) + "#");
         }
     }
 
     public static void printSideWays(int size) {
         for (int i = 0; i < size; i++) {
-            if (i == 0)
-                System.out.println("#".repeat(size));
-            else if (i == size - 1)
-                System.out.println(" ".repeat(i) + "#".repeat(size));
-            else
-                System.out.println(" ".repeat(i) + "#" + " ".repeat(size - 2) + "#");
+            if (i == 0) System.out.println("#".repeat(size));
+            else if (i == size - 1) System.out.println(" ".repeat(i) + "#".repeat(size));
+            else System.out.println(" ".repeat(i) + "#" + " ".repeat(size - 2) + "#");
         }
     }
 
     public static void printByLarging(int size) {
-        for (int i = 0; i < size; i++) {
-            if (i == 0)
-                System.out.println("#");
-            else {
-                System.out.println(" ".repeat(i) + "#".repeat(i+1));
-            }
+        String text = "#";
+        int counter = size * (size + 1) / 2;
+        for (int i = 1; i <= size;i++) {
+            counter -= i;
+            System.out.println(" ".repeat(counter) + text.repeat(i));
         }
     }
 
