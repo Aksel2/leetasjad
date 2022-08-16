36b844c75ef6b9ae2778a43533fb30cf295454e2
Aksel Õim <akseloim@gmail.com>
2022-08-16 13:43:21 +0300

    Lahendused

--- Easy/Solution21.java
+++ Easy/Solution21.java
@@ -57,6 +57,8 @@ class Solution21 {
     public static void main(String[] args) {
         ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-        System.out.println(mergeTwoLists(list1, list2).next.next.next.next.next.val);
+        int x = 1, y = 1;
+        System.out.println(x+++y);
+        //System.out.println(mergeTwoLists(list1, list2).next.next.next.next.next.val);
     }
 }
--- Easy/Solution387.java
+++ Easy/Solution387.java
@@ -1,2 +1,34 @@
-package Easy;public class Solution387 {
+package Easy;
+
+import java.util.HashMap;
+
+public class Solution387 {
+
+    public static int firstUniqChar(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            String x = s.charAt(i) + "";
+            if (s.indexOf(x) == s.lastIndexOf(x))
+                return i;
+        }
+        return -1;
+    }
+
+    public static int hashFirstUniqChar(String s) {
+        HashMap<Character, Integer> hash = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            hash.put(c, hash.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (hash.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "sdaghufdsaif";
+        System.out.println(firstUniqChar(s));
+        System.out.println(hashFirstUniqChar(s));
+    }
 }
